38eb3dde1326dc536500808700f9e67bb291a67b
Henry <pienry3g@gmail.com>
2018-11-27 15:15:37 +0100

    [Chore #162171013] Setup unit testing
    - Install chai
    - Install chai-http
    - Install mocha
    - Install mocha-lcov-reporter
    - Create test/index.js as the test entry point
    - Create test/users.js, test/likes.js, test/comments.js, test/articles.js
    
    [Finishes #162171012]

---
+++ test/index.js
@@ -0,0 +1,10 @@
+/**
+ * This file will server as the index to import all unit test in the order in which the testing should take place. 
+ */
+
+ /*
+import './users.test';
+import './articles.test';
+import './likes.test';
+import './comments.test';
+*/
