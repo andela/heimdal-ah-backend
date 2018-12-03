c698b6a5a3296ae7abbd5f0bbb5c70006f3dd1b4
Henry <pienry3g@gmail.com>
2018-12-03 13:37:50 +0100

    Update index.js and handle new test scenario
    - Write test to handle any random endpoint
    - Add an endpoint to index.js
    - Update package.json

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import bodyParser from 'body-parser';
 import nodeLogger from 'logger';
 
 // Import The Routes Index File =========================================
-import routes from './routes/index';
+import routes from './routes';
 
 const logger = nodeLogger.createLogger();
 const PORT = process.env.PORT || 4000;
@@ -16,6 +16,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/api/', routes);
+
+// Default to here when an invalid endpoint is entered
+app.use('/', (req, res) => res.status(200).json({
+  success: true,
+  message: 'Welcome to Authors Haven by Heimdal'
+}));
 
 app.listen(PORT, () => {
   logger.info(`connected on port ${PORT}`);
