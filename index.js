d3b33274d6e1c9138e86049964d76cfba935381b
el-Joft <ottimothy@gmail.com>
2018-12-04 15:19:14 +0100

    #162171017 User receives a descriptive signup/registration validation errors (#20)
    * Add Heroku generated app.json
    
    * [Chore #162171015] implement user registration with jwt
    
    * [Chore #162171015] implement user registration with jwt
    
    * [Chore #162171015] implement user registration with jwt
    
    *  [chore #162171015 ] implement login feature
    
    * [Feature #162171017] User recieves descriptive signup/registration validation errors
    
    * [Feature ##162171017] Refactoring the test file and start script
    
    * [Feature ##162171017] Changed the name of the class in lib/user from UserLibFile to UserModelQuery
    
    * [Feature #162171015]Refactor status responses
    
    * [Feature #162171015]install sequelize dependency
    
    * [Feature #162171015]tadd test script to travis yml
    
    * [Feature #162171015]saved token to req.app
    
    * Feature #162171015]edit travis yml
    
    * [Feature #162171017] Fixing Hound Violations

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import nodeLogger from 'logger';
+import validator from 'express-validator';
+import routes from './routes';
 
 const logger = nodeLogger.createLogger();
 const PORT = process.env.PORT || 4000;
@@ -11,7 +13,11 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(validator());
+app.use('/api/v1/auth', routes);
 
 app.listen(PORT, () => {
   logger.info(`connected on port ${PORT}`);
 });
+
+export default app;
--- test/users.js
+++ test/users.js
@@ -1,7 +1,55 @@
-// import chai from 'chai';
+// import chai, { expect } from 'chai';
 // import chaiHttp from 'chai-http';
 // import index from '../index';
 
-// chai.use(chaiHttp);
+// // const [expect] = [chai.expect];
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../index';
 
-// const [expect] = [chai.expect];
+chai.should();
+
+chai.use(chaiHttp);
+
+describe('Test for registering a new user', () => {
+  it('should return 201 on sucessfully creating a new user', async () => {
+    const data = {
+      email: 'testin@test.com',
+      password: 'etydhfkjdkvl1',
+      username: 'test'
+    };
+    const res = await chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(data);
+    res.should.be.a('object');
+    res.status.should.equal(201);
+  });
+  // it('should return 409 if user already exists', async () => {
+  //   const data = {
+  //     email: 'testin@test.com',
+  //     password: 'etydhfkjdkvl1',
+  //     username: 'test'
+  //   };
+  //   const res = await chai.request(app)
+  //     .post('/api/v1/auth/signup')
+  //     .send(data);
+  //   res.should.be.a('object');
+  //   res.status.should.equal(409);
+  // });
+  it('should return error if user enters an existing email', async () => {
+    const userDataWithAnExistingEmail = {
+      email: 'testin@test.com',
+      password: 'omotayo123',
+      username: 'Omotayo'
+    };
+
+    const res = await chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(userDataWithAnExistingEmail);
+    res.status.should.equal(409);
+    res.body.should.be.a('object');
+    res.body.should.be.a('object');
+    res.body.should.have.property('message');
+    res.body.message.should.equal('This email has been taken');
+  });
+});
