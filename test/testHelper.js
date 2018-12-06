9fd90b0bb6c2c2de665165870808772a49fb967f
Henry <pienry3g@gmail.com>
2018-12-06 19:08:21 +0100

    [Feature #162171016] Implement Feedback

--- test/testHelper.js
+++ test/testHelper.js
@@ -8,6 +8,7 @@ const testHelper = {
   password: '12345',
   randomValidToken:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImZseWJveS5taWNoZWFsQGdtYWlsLmNvbSIsImlhdCI6MTU0Mzc2ODI1NSwiZXhwIjoxNTQzOTQxMDU1fQ.dtmuomhi1F-AtW8KoTEx7GSmm1DOcMOm9q_40G4HU9g',
+  invalidToken: 'qwerty'
 };
 
 export default testHelper;
