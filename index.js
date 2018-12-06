6ec088e376295b6313523fb55cb16dcda13f14e0
Oluwaseun Jolaoso <oluwaseunjolaoso@Oluwaseuns-MacBook-Pro.local>
2018-12-06 11:09:40 +0100

    fixing travis fails

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ import express from 'express';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import validator from 'express-validator';
-// import session from 'express-session';
+import session from 'express-session';
 
 import { auth, profiles, user } from './routes';
 import logger from './config/logger';
-// import passportAuth from './config/passportAuth';
+import passportAuth from './config/passportAuth';
 
 const PORT = process.env.PORT || 4000;
 
@@ -17,17 +17,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(validator());
 
-// app.use(session({
-//   secret: process.env.SESSION_SECRET,
-//   resave: true,
-//   saveUninitialized: true,
-// }));
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+}));
 
-// passportAuth(app);
 
 app.use('/api/v1/auth', auth);
 app.use('/api/v1/profiles', profiles);
 app.use('/api/v1/forgotpassword', user);
+// passportAuth(app);
 
 app.use((req, res) => res.status(404).json('not found'));
 
