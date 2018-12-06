b9687466125535eea303d8ff0b76a9d38acca694
Henry <pienry3g@gmail.com>
2018-12-06 19:08:10 +0100

    [Feature #162171016] Implement Feedback

---
+++ test/testHelper.js
@@ -0,0 +1,13 @@
+const testHelper = {
+  /**
+   * @description - This class handles the users
+   * */
+
+  existingEmail: undefined,
+  userEmail: 'henry.izontimi@gmail.com',
+  password: '12345',
+  randomValidToken:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImZseWJveS5taWNoZWFsQGdtYWlsLmNvbSIsImlhdCI6MTU0Mzc2ODI1NSwiZXhwIjoxNTQzOTQxMDU1fQ.dtmuomhi1F-AtW8KoTEx7GSmm1DOcMOm9q_40G4HU9g',
+};
+
+export default testHelper;
