5bf583082c10dad9723632eafcb76e221fc0eded
henperi <36136977+henperi@users.noreply.github.com>
2018-12-11 13:32:11 +0100

    #162171016 Refactor email verification functionality 162171016 (#30)
    * [chore] Refactor cerification mail functionality
    
    - Remove expiry time from email token
    - Abstract the email verification body, subject and process to a method inside mailer.
    -
    
    * chore] Refactor email verification functionality
    
    - Make the user db email field a boolean type
    - Remove expiry time from email token
    - Abstract the email verification body, subject and process to a method inside mailer.
    
    * [chore] Remove unnecessary comments
    
    * [chore] Remove unused var from helper/mail.js
    
    * [chore 162171016] update helper/mailer.js

--- models/users.js
+++ models/users.js
@@ -1,46 +1,48 @@
-
 export default (sequelize, DataTypes) => {
-  const Users = sequelize.define('users', {
-    email: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      unique: true,
+  const Users = sequelize.define(
+    'users',
+    {
+      email: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        unique: true
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      facebookId: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      googleId: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      twitterId: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      emailVerification: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+      },
+      resettingPassword: DataTypes.BOOLEAN
     },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    facebookId: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    googleId: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    twitterId: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    emailVerification: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-  }, {});
+    {}
+  );
   Users.associate = (models) => {
     // associations can be defined here
     Users.belongsTo(models.roles, {
       foreignKey: 'roleId',
       onDelete: 'CASCADE',
-      as: 'roles',
+      as: 'roles'
     });
-    Users.hasOne(models.profiles, {
-
-    });
+    Users.hasOne(models.profiles, {});
   };
   return Users;
 };
