b3be557f1c35a0446bba174e1aa568494521af94
victor obije <konigeobije@gmail.com>
2018-12-12 19:06:43 +0100

    [feature 162171034] updated branch

--- seeders/20181211175433-users-seeder.js
+++ seeders/20181211175433-users-seeder.js
@@ -4,10 +4,6 @@ export default {
   up: (queryInterface, Sequelize) => queryInterface.bulkInsert('users', [{
     email: 'admin@heimdal.com',
     password: '123456',
-    username: 'admin',
-    facebookId: null,
-    googleId: null,
-    twitterId: null,
     emailVerification: true,
     createdAt: new Date(),
     updatedAt: new Date(),
@@ -16,10 +12,6 @@ export default {
   {
     email: 'user@heimdal.com',
     password: '123456',
-    username: 'user',
-    facebookId: null,
-    googleId: null,
-    twitterId: null,
     emailVerification: true,
     createdAt: new Date(),
     updatedAt: new Date(),
@@ -29,10 +21,6 @@ export default {
   {
     email: 'publisher@heimdal.com',
     password: '$2a$08$Pg2qjiDZGPk/EpqZM2GB6u06MTMfsuOuVYxEYlH/p7F7/bjwqP3eu',
-    username: 'publisher',
-    facebookId: null,
-    googleId: null,
-    twitterId: null,
     emailVerification: true,
     createdAt: new Date(),
     updatedAt: new Date(),
