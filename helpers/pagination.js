c019d8d7e269793a8e856dc05ae49aeb1df51c3a
pitaz <peter.odekwo@gmail.com>
2018-12-17 16:50:40 +0100

    [feature #162171029] increase articles count limit to 20 in pagination file

--- helpers/pagination.js
+++ helpers/pagination.js
@@ -1,7 +1,7 @@
 const pagination = (page, size, count) => {
   let currentPage = Math.abs(Number(page)) || 1;
   let offset = 0;
-  let limit = 2;
+  let limit = 20;
   const sizeNo = Number(size);
   if (!Number.isNaN(sizeNo) && sizeNo > 0) limit = size;
   const totalPages = Math.ceil(count / limit);
