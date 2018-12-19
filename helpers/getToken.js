8b0da829ca7c7f7e0a36826098af408962bdbc31
pitaz <peter.odekwo@gmail.com>
2018-12-19 18:20:54 +0100

    [feature #162171038] resolve hound violations

--- helpers/getToken.js
+++ helpers/getToken.js
@@ -1,13 +1,15 @@
 import jwt from 'jsonwebtoken';
 
-const getToken = (userId, username, roleId) => jwt.sign({
-  userId,
-  username,
-  roleId
-},
-process.env.TOKEN_SECRET,
-{
-  expiresIn: 86400
-});
+const getToken = (userId, username, roleId) => jwt.sign(
+  {
+    userId,
+    username,
+    roleId
+  },
+  process.env.TOKEN_SECRET,
+  {
+    expiresIn: 86400
+  }
+);
 
 export default getToken;
