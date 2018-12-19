a4e0671b1ec9fb618cf47b16fa6dc16598a59d4f
pitaz <peter.odekwo@gmail.com>
2018-12-19 18:45:00 +0100

    [feature #162171038] fix errors in article controller
    - pass title  parameter in ArticleQueryModel function

--- controllers/ArticlesController.js
+++ controllers/ArticlesController.js
@@ -29,7 +29,7 @@ class ArticlesController {
       tags, body, title, description, image
     } = req.body;
     try {
-      const articleTitle = await ArticleQueryModel.getArticleByTitle();
+      const articleTitle = await ArticleQueryModel.getArticleByTitle(req.body.title);
       const articleSlug = checkTitle(req.body.title, articleTitle);
       const readingTime = calcReadingTime(body);
       const newArticle = await Article.create({
