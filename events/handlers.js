e1f18fe05302c0a2e82a561e01428932f3d8758a
victor obije <konigeobije@gmail.com>
2018-12-21 10:06:31 +0100

    [feature 162171027] updated files

--- events/handlers.js
+++ events/handlers.js
@@ -8,21 +8,31 @@ const { updateHighlights } = highlightsLogic;
 
 const handlers = {
   register(io) {
-    eventEmitter.on(eventTypes.ARTICLE_NOTIFICATION_EVENT,
-      payload => Notifications.addNotification(payload, io));
+    eventEmitter.on(
+      eventTypes.ARTICLE_NOTIFICATION_EVENT,
+      payload => Notifications.addNotification(payload, io)
+    );
 
-    eventEmitter.on(eventTypes.COMMENT_NOTIFICATION_EVENT,
-      payload => Notifications.addNotification(payload, io));
+    eventEmitter.on(
+      eventTypes.COMMENT_NOTIFICATION_EVENT,
+      payload => Notifications.addNotification(payload, io)
+    );
 
-    eventEmitter.on(eventTypes.RATING_INTERACTION_EVENT,
-      payload => Notifications.addNotification(payload, io));
+    eventEmitter.on(
+      eventTypes.RATING_INTERACTION_EVENT,
+      payload => Notifications.addNotification(payload, io)
+    );
 
-    eventEmitter.on(eventTypes.FOLLOW_INTERACTION_EVENT,
-      payload => Notifications.followNotification(payload, io));
+    eventEmitter.on(
+      eventTypes.FOLLOW_INTERACTION_EVENT,
+      payload => Notifications.followNotification(payload, io)
+    );
 
-    eventEmitter.on(eventTypes.UPDATEHIGHLIGHT_EVENT, (highlightedPortions, body, userId) => {
-      updateHighlights(highlightedPortions, body, userId);
-    });
+    eventEmitter.on(
+      eventTypes.UPDATEHIGHLIGHT_EVENT, (highlightedPortions, body, userId) => {
+        updateHighlights(highlightedPortions, body, userId);
+      }
+    );
   }
 };
 
