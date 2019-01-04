6db26bfb595b28a88417cf16dae44eae455e4d06
pitaz <peter.odekwo@gmail.com>
2019-01-04 10:08:36 +0100

    add admin seed file

--- seeders/20181211173050-users-seeder.js
+++ seeders/20181211173050-users-seeder.js
@@ -5,14 +5,6 @@ const genSalt = bcrypt.genSaltSync(8);
 const hashPassword = bcrypt.hashSync('12345678heimdal', genSalt);
 export default {
   up: (queryInterface, Sequelize) => queryInterface.bulkInsert('users', [{
-    email: 'admin@heimdal.com',
-    password: hashPassword,
-    emailVerification: true,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    roleId: 1,
-  },
-  {
     email: 'user@heimdal.com',
     password: hashPassword,
     emailVerification: true,
