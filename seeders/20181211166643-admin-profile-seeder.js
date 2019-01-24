f833bb1d171d9b428d6a06c290fed2487e754b80
sudhons <sudhons@gmail.com>
2019-01-24 18:21:50 +0100

    [Feature 163387260] updates seed files

--- seeders/20181211166643-admin-profile-seeder.js
+++ seeders/20181211166643-admin-profile-seeder.js
@@ -1,8 +1,12 @@
 /* eslint-disable no-unused-vars */
+import Chance from 'chance';
+
+const chance = new Chance();
+
 export default {
   up: (queryInterface, Sequelize) => queryInterface.bulkInsert('profiles', [{
     username: 'wale',
-    biodata: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore molestias aspernatur temporibus est fuga culpa saepe ipsa similique numquam adipisci doloremque, voluptatum suscipit sapiente provident odio! Placeat voluptatem nisi voluptates! Optio officiis rerum neque, provident eos nihil mollitia reprehenderit error modi incidunt minima blanditiis nostrum amet totam quam a assumenda? Corrupti nam maiores possimus quisquam sed magni officiis consequatur praesentium nobis, quae delectus aut tenetur iste tempora molestiae rerum, iusto quis nisi officia amet? Fuga, quos molestias? Eius iusto tempore quisquam vitae blanditiis, voluptates magni eos? Omnis beatae, eligendi iste cumque nihil aliquam sint autem, sequi commodi ratione ipsum voluptas!Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore molestias aspernatur temporibus est fuga culpa saepe ipsa similique numquam adipisci doloremque, voluptatum suscipit sapiente provident odio! Placeat voluptatem nisi voluptates! Optio officiis rerum neque, provident eos nihil mollitia reprehenderit error modi incidunt minima blanditiis nostrum amet totam quam a assumenda? Corrupti nam maiores possimus quisquam sed magni officiis consequatur praesentium nobis, quae delectus aut tenetur iste tempora molestiae rerum, iusto quis nisi officia amet? Fuga, quos molestias? Eius iusto tempore quisquam vitae blanditiis, voluptates magni eos? Omnis beatae, eligendi iste cumque nihil aliquam sint autem, sequi commodi ratione ipsum voluptas!',
+    biodata: chance.sentence({ words: 100 }),
     firstName: 'Ayandiran',
     lastName: 'Wale',
     image: 'https://hardwaremassive.com/sites/default/files/avatar/users/ian_square_profile_picture.jpg',
--- seeders/20181212033227-profiles-seeder.js
+++ seeders/20181212033227-profiles-seeder.js
@@ -1,9 +1,12 @@
 /* eslint-disable no-unused-vars */
+import Chance from 'chance';
+
+const chance = new Chance();
 
 export default {
   up: (queryInterface, Sequelize) => queryInterface.bulkInsert('profiles', [{
     username: 'segun',
-    biodata: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore molestias aspernatur temporibus est fuga culpa saepe ipsa similique numquam adipisci doloremque, voluptatum suscipit sapiente provident odio! Placeat voluptatem nisi voluptates! Optio officiis rerum neque, provident eos nihil mollitia reprehenderit error modi incidunt minima blanditiis nostrum amet totam quam a assumenda? Corrupti nam maiores possimus quisquam sed magni officiis consequatur praesentium nobis, quae delectus aut tenetur iste tempora molestiae rerum, iusto quis nisi officia amet? Fuga, quos molestias? Eius iusto tempore quisquam vitae blanditiis, voluptates magni eos? Omnis beatae, eligendi iste cumque nihil aliquam sint autem, sequi commodi ratione ipsum voluptas!Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore molestias aspernatur temporibus est fuga culpa saepe ipsa similique numquam adipisci doloremque, voluptatum suscipit sapiente provident odio! Placeat voluptatem nisi voluptates! Optio officiis rerum neque, provident eos nihil mollitia reprehenderit error modi incidunt minima blanditiis nostrum amet totam quam a assumenda? Corrupti nam maiores possimus quisquam sed magni officiis consequatur praesentium nobis, quae delectus aut tenetur iste tempora molestiae rerum, iusto quis nisi officia amet? Fuga, quos molestias? Eius iusto tempore quisquam vitae blanditiis, voluptates magni eos? Omnis beatae, eligendi iste cumque nihil aliquam sint autem, sequi commodi ratione ipsum voluptas!',
+    biodata: chance.sentence({ words: 100 }),
     firstName: 'Sunny',
     lastName: 'Edogbo',
     image: 'http://telecommsult.nl/wp-content/uploads/2016/07/brad-profile-square.jpg',
@@ -13,7 +16,7 @@ export default {
   },
   {
     username: 'john',
-    biodata: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore molestias aspernatur temporibus est fuga culpa saepe ipsa similique numquam adipisci doloremque, voluptatum suscipit sapiente provident odio! Placeat voluptatem nisi voluptates! Optio officiis rerum neque, provident eos nihil mollitia reprehenderit error modi incidunt minima blanditiis nostrum amet totam quam a assumenda? Corrupti nam maiores possimus quisquam sed magni officiis consequatur praesentium nobis, quae delectus aut tenetur iste tempora molestiae rerum, iusto quis nisi officia amet? Fuga, quos molestias',
+    biodata: chance.sentence({ words: 100 }),
     firstName: 'Jonathan',
     lastName: 'Edwards',
     image: 'http://sunrift.com/wp-content/uploads/2014/12/Blake-profile-photo-square.jpg',
@@ -23,7 +26,7 @@ export default {
   },
   {
     username: 'james',
-    biodata: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore molestias aspernatur temporibus est fuga culpa saepe ipsa similique numquam adipisci doloremque, voluptatum suscipit sapiente provident odio! Placeat voluptatem nisi voluptates! Optio officiis rerum neque, provident eos nihil mollitia reprehenderit error modi incidunt minima blanditiis nostrum amet totam quam a assumenda? Corrupti nam maiores possimus quisquam sed magni officiis consequatur praesentium nobis, quae delectus aut tenetur iste tempora molestiae rerum, iusto quis nisi officia amet? Fuga, quos molestias? Eius iusto tempore quisquam vitae blanditiis, voluptates magni eos? Omnis beatae, eligendi iste cumque nihil aliquam sint autem, sequi commodi ratione ipsum voluptas!Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore molestias aspernatur temporibus est fuga culpa saepe ipsa similique numquam adipisci doloremque, voluptatum suscipit sapiente provident odio! Placeat voluptatem nisi voluptates!',
+    biodata: chance.sentence({ words: 100 }),
     firstName: 'Victor',
     lastName: 'Obije',
     image: 'http://www.samra.co.za/wp-content/uploads/2015/04/Andy-Hadfield-Profile-SQUARE-Low-Res.jpg',
@@ -33,7 +36,7 @@ export default {
   },
   {
     username: 'joe',
-    biodata: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore molestias aspernatur temporibus est fuga culpa saepe ipsa similique numquam adipisci doloremque, voluptatum suscipit sapiente provident odio! Placeat voluptatem nisi voluptates! Optio officiis rerum neque, provident eos nihil mollitia reprehenderit error modi incidunt minima blanditiis nostrum amet totam quam a assumenda? Corrupti nam maiores possimus quisquam sed magni officiis consequatur praesentium nobis, quae delectus aut tenetur iste tempora molestiae rerum, iusto quis nisi officia amet? Fuga, quos molestias? Eius iusto tempore quisquam vitae blanditiis, voluptates magni eos? Omnis beatae, eligendi iste cumque nihil aliquam sint autem, sequi commodi ratione ipsum voluptas!Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore molestias aspernatur temporibus est fuga culpa saepe ipsa similique numquam adipisci doloremque, voluptatum suscipit sapiente provident odio! Placeat voluptatem nisi voluptates! Optio officiis rerum neque, provident eos nihil mollitia reprehenderit error modi incidunt minima blanditiis nostrum amet totam quam a assumenda? Corrupti nam maiores possimus quisquam sed magni officiis consequatur praesentium nobis, quae delectus aut tenetur iste tempora molestiae rerum, iusto quis nisi officia amet? Fuga, quos molestias? Eius iusto tempore quisquam vitae blanditiis, voluptates magni eos? Omnis beatae, eligendi iste cumque nihil aliquam sint autem, sequi commodi ratione ipsum voluptas!',
+    biodata: chance.sentence({ words: 100 }),
     firstName: 'Pereowei',
     lastName: 'Izontimi',
     image: 'https://static1.squarespace.com/static/5249245ae4b0c38c3f1d6415/t/5bad0c248165f5f3c2e122ad/1538067498769/1.+Kurt+square+profile+pic+updated.JPG?format=300w',
@@ -43,7 +46,7 @@ export default {
   },
   {
     username: 'user1',
-    biodata: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore molestias aspernatur temporibus est fuga culpa saepe ipsa similique numquam adipisci doloremque, voluptatum suscipit sapiente provident odio! Placeat voluptatem nisi voluptates! Optio officiis rerum neque, provident eos nihil mollitia reprehenderit error modi incidunt minima blanditiis nostrum amet totam quam a assumenda? Corrupti nam maiores possimus quisquam sed magni officiis consequatur praesentium nobis, quae delectus aut tenetur iste tempora molestiae rerum, iusto quis nisi officia amet? Fuga, quos molestias? Eius iusto tempore quisquam vitae blanditiis, voluptates magni eos? Omnis beatae, eligendi iste cumque nihil aliquam sint autem, sequi commodi ratione ipsum voluptas!Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore molestias aspernatur temporibus est fuga culpa saepe ipsa similique numquam adipi',
+    biodata: chance.sentence({ words: 100 }),
     firstName: 'Timothy',
     lastName: 'Omotayo',
     image: 'http://d38we5ntdyxyje.cloudfront.net/295552/profile/avatar_medium_square.jpg',
